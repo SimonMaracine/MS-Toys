a3d0443a58c6c34bd047f06074885520482d3fe4
SimonMaracine <simonmaracine@gmail.com>
2023-01-15 23:19:45 +0200

    Add models and some database code

---
+++ MS-Toys/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using MS_Toys.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MS_Toys.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        [MaxLength(30)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string EncryptedPassword { get; set; }  // TODO Encrypted
+    }
+}
