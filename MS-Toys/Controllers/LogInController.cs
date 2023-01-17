d83aafc2d47d96d96df70a7f9ecf48ad96d910c6
malinnn <pappmalin@gmail.com>
2023-01-17 22:27:26 +0200

    Add Log In tab

---
+++ MS-Toys/Controllers/LogInController.cs
@@ -0,0 +1,25 @@
+﻿using MS_Toys.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MS_Toys.Controllers
+{
+    public class LogInController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        
+        public ActionResult Create([Bind(Include = "Username, EncryptedPassword")] User user)
+        {
+            return View(user);
+        } 
+    }
+}
